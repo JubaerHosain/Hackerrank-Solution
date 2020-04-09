14f8a0c4d90c60c2ccc8713bbac8a4afd4b510fe
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-04-09 11:29:12 +0600

    Create Insertion Sort - Part 2.java

---
+++ Algorithms/Insertion Sort - Part 2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the insertionSort2 function below.
+    static void insertionSort2(int n, int[] arr) {
+        int i, j, key;
+        
+        for(i = 1; i < arr.length; i++){
+            key = arr[i];
+            j = i - 1;
+
+            while(j >=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+            printArray(arr);
+        }
+
+    }
+
+    static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort2(n, arr);
+
+        scanner.close();
+    }
+}
