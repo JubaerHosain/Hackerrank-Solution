7b550a4ef2e4de5ecbb38302eda0e5af79826d1f
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-06-04 13:09:52 +0600

    Create Is This a Binary Search Tree?.java

---
+++ Data Structures/Is This a Binary Search Tree?.java
@@ -0,0 +1,13 @@
+boolean checkBST(Node root) {
+    return checkBST(root, 0, 10000);
+}
+
+boolean checkBST(Node node, int min, int max) {
+    if (node == null) {
+        return true;
+    } else if (node.data < min || node.data > max) { // we assume BST cannot have duplicate values
+        return false;
+    } else {
+        return checkBST(node.left, min, node.data - 1) && checkBST(node.right, node.data + 1, max);
+    }
+}
