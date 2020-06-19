8eb40d9aafe7d3032496b83692a0b310c6770794
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-06-19 10:02:06 +0600

    Create Reverse a doubly linked list.java

---
+++ Interview Preparation Kit/Reverse a doubly linked list.java
@@ -0,0 +1,34 @@
+
+
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        DoublyLinkedListNode temp = null;
+        DoublyLinkedListNode current = head;
+
+        while(current != null){
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+
+        /* Before changing head, check for the cases like empty  
+        list and list with only one node */
+        if(temp != null){
+            head = temp.prev;
+        }
+
+        return head;
+    }
+
