a0d9c597121395aad9810644dafb79093cc68ec1
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-08-17 16:02:21 +0600

    Update Forward References.java

--- Regex/Forward References.java
+++ Regex/Forward References.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("^tac(tac(tic)?)*$"); // Use \\ instead of using \ 
+        tester.checker("^tac(tac(tic)?)*$"); // ^(\\2tic|(tac))+$
     
     }
 }
