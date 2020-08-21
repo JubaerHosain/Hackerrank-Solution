036a1b41dad1dcaa3c53ffb3b6921dbc5c17c52b
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-08-21 20:59:20 +0600

    Create Find a Word.java

---
+++ Regex/Find a Word.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        scan.nextLine();
+        String[] lines = new String[n];
+
+        for(int i = 0; i < n; i++){
+            lines[i] = scan.nextLine();
+        }
+
+        int q = scan.nextInt();
+        scan.nextLine();
+
+        while(q-- > 0){
+            String word = scan.next();
+            int oc = 0;
+
+            Pattern p = Pattern.compile("(?<!\\w)"+word+"(?!\\w)");
+            for(String line: lines){
+                Matcher m = p.matcher(line);
+
+                while(m.find()){
+                    oc++;
+                }
+            }
+
+            System.out.println(oc);
+        }
+
+        scan.close();
+    }
+}
