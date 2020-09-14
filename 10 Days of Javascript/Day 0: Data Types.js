ae9a3a21fbf3cf40df4087901b5abccf85b6f391
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2020-09-14 23:27:01 +0600

    Create Day 0: Data Types.js

---
+++ 10 Days of Javascript/Day 0: Data Types.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    console.log(firstInteger + parseInt(secondInteger));  //+operator convert string to number (+secondInteger)
+
+
+    const firstDecimal = 4.0;
+    console.log(firstDecimal + parseFloat(secondDecimal)); //+operator convert string to number (+secondDecimal)
+    
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+    
+    
+}
+
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
